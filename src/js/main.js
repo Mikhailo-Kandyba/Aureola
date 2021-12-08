248667460be78e814bd183a55f77a388e02fc5d5
Mikhailo <mikhailo.kandyba0@gmail.com>
2021-12-08 23:35:17 +0300

    add menu and banner

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+
 $(".btn-menu").on('click', function(e) {
     e.preventDefault();
     $(this).toggleClass("active");
@@ -5,6 +6,21 @@ $(".btn-menu").on('click', function(e) {
     $("body").toggleClass('overflow-hidden');
 });
 
+$('.menu-navigation-wrap a').click(function(){
+    $('.btn-menu').removeClass("active");
+    $(".menu-navigation-wrap").removeClass('show-menu');
+    $("body").removeClass('overflow-hidden');
+});
 
+$('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+});
 
 
