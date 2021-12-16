4ebb38b18971cce8b51357916e8462e9edb71a54
Mikhailo <mikhailo.kandyba0@gmail.com>
2021-12-16 22:11:13 +0300

    bugs fixing

--- src/js/main.js
+++ src/js/main.js
@@ -70,7 +70,7 @@ $('.new-items').slick({
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 4,
+                slidesToShow: 3,
                 slidesToScroll: 1,
                 infinite: true,
                 dots: true
